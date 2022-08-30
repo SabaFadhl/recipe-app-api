2a645c8a86285bb4771f28f2241b23ed5f61790e
SabaFadhl <sabafadl98@gmail.com>
2022-08-30 13:39:05 +0300

    fix flake8 errors

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -5,9 +5,11 @@ import time
 from psycopg2 import OperationalError as Psycopg2OpError
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
+
+
 class Command(BaseCommand):
     """ Django command to wait for database"""
-    def handle(self,*args,**options):
+    def handle(self, *args, **options):
         """Entrypoint for command."""
         self.stdout.write('Waiting for database...')
         db_up = False
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -3,6 +3,8 @@ Tests for models.
 """
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+
+
 class ModelTests(TestCase):
     """Test models."""
     def test_create_user_with_email_successful(self):
