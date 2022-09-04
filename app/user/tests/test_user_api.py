d168bc4d30b15481c0989e7516e51a835a39f8e7
SabaFadhl <sabafadl98@gmail.com>
2022-09-04 10:12:25 +0300

    fix flake8 rules

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -17,10 +17,13 @@ ME_URL = reverse('user:me')
 def create_user(**params):
     """Create and return a new user."""
     return get_user_model().objects.create_user(**params)
+
+
 class PublicUserApiTests(TestCase):
     """Test the public features of the user API."""
     def setUp(self):
         self.client = APIClient()
+
     def test_create_user_success(self):
         """Test creating a user is successful."""
         payload = {
@@ -30,9 +33,11 @@ class PublicUserApiTests(TestCase):
         }
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
         user = get_user_model().objects.get(email=payload['email'])
         self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
+
     def test_user_with_email_exists_error(self):
         """Test error returned if user with email exists."""
         payload = {
@@ -40,9 +45,11 @@ class PublicUserApiTests(TestCase):
             'password': 'testpass123',
             'name': 'Test Name',
         }
+
         create_user(**payload)
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
     def test_password_too_short_error(self):
         """Test an error is returned if password less than 5 chars."""
         payload = {
@@ -104,17 +111,11 @@ class PublicUserApiTests(TestCase):
     def test_retrieve_user_unauthorized(self):
         """Test authentication is required for users."""
         res = self.client.get(ME_URL)
-
-
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
-
-
 
 
 class PrivateUserApiTests(TestCase):
     """Test API requests that require authentication."""
-
-
     def setUp(self):
         self.user = create_user(
             email='test@example.com',
@@ -124,11 +125,9 @@ class PrivateUserApiTests(TestCase):
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
 
-
     def test_retrieve_profile_success(self):
         """Test retrieving profile for logged in user."""
         res = self.client.get(ME_URL)
-
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, {
@@ -136,22 +135,17 @@ class PrivateUserApiTests(TestCase):
             'email': self.user.email,
         })
 
-
     def test_post_me_not_allowed(self):
         """Test POST is not allowed for the me endpoint."""
         res = self.client.post(ME_URL, {})
 
-
         self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-
 
     def test_update_user_profile(self):
         """Test updating the user profile for the authenticated user."""
         payload = {'name': 'Updated name', 'password': 'newpassword123'}
 
-
         res = self.client.patch(ME_URL, payload)
-
 
         self.user.refresh_from_db()
         self.assertEqual(self.user.name, payload['name'])
--- app/user/views.py
+++ app/user/views.py
@@ -22,13 +22,11 @@ class CreateTokenView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
-
 class ManageUserView(generics.RetrieveUpdateAPIView):
     """Manage the authenticated user."""
     serializer_class = UserSerializer
     authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
-
 
     def get_object(self):
         """Retrieve and return the authenticated user."""
